472a968d71ccd782906f19c69756ecb937fe69e5
Samuel Donovan <samueldnvn4@gmail.com>
2024-02-05 13:49:42 -0500

    added basic plotting functionality

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function addTileLayer(Map) {
 }
 
 function addMarker(lat, long, personInfo) {
-  let point = L.marker([lat, long], {icon: myIcon}).addTo(Map);
+  let point = L.marker([lat, long]).addTo(Map);
   point.bindPopup(`<b>${personInfo}<b>`);
 }
 
@@ -30,23 +30,56 @@ btn.onclick = function () {
 };
 
 
+
+
+
+let cursorCoordinates = null;
+Map.on('mousemove', function(event) {
+    cursorCoordinates = event.latlng;
+});
+function getCoordinates() {
+  return {
+    mapLat:cursorCoordinates.lat, 
+    mapLng:cursorCoordinates.lng
+  }
+}
+
+
+
 // Right Click Menu
 const contextMenu = document.querySelector(".right-click-wrapper");
+
+const addLocationMenu = document.querySelector(".location-menu-wrapper");
+const sightingTypeInput = document.querySelector("#location-sighting-type");
+const addLocationPostButton = document.querySelector("#location-post-button");
+
 const styledMap = document.querySelector("#map");
 document.addEventListener("contextmenu", e => {
   e.preventDefault();
-  let x = e.offsetX, y = e.offsetY;
+  let x = e.pageX, y = e.pageY;
+
   if (e.target == styledMap){
-    console.log("map clicked");
     contextMenu.style.left = `${x}px`
     contextMenu.style.top = `${y}px`
     contextMenu.style.visibility = "visible";
+
+    const addLocationButton = document.querySelector("#right-click-location");
+    addLocationButton.onclick = function () {
+      addLocationMenu.style.visibility = "visible";
+      contextMenu.style.visibility = "hidden";
+    }
   }
   else {
     contextMenu.style.left = `${x}px`
     contextMenu.style.top = `${y}px`
     contextMenu.style.visibility = "visible";
-    console.log("right clicked");
+
 
   }
 })
+
+addLocationPostButton.onclick = function () {
+
+  addMarker(getCoordinates().mapLat, getCoordinates().mapLng, sightingTypeInput.value);
+  addLocationMenu.style.visibility = "hidden";
+}
